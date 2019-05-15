b5cca3e0ba8c4cc11e301b0088f38f35d0e2b51f
Kyle Brannon <kylebrannon@railserve.biz>
2019-05-15 17:16:27 -0400

    Fixed text display upon Comprehensive Test Completion Added script to detect enabling of certificate Added Ice material to walls in the second zone of the Comprehensive Test

---
+++ MergedProject/Assets/Scripts/Winrar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Winrar : MonoBehaviour {
+	
+	public Text guideText;
+	public string winText = "Congratulations!  You've completed all available training courses!";
+	
+	void OnEnable () {
+		guideText.text = winText;
+	}
+}
